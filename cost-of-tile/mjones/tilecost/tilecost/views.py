217e811a456c761e4b0ad6cb2340ad59ebdc4b2d
Mike Jones LDAP account <mjones@mjonesstage001.shopzilla.laxhq>
2013-08-16 11:16:42 -0700

    Bit of documentation, small cleanups

--- cost-of-tile/mjones/tilecost/tilecost/views.py
+++ cost-of-tile/mjones/tilecost/tilecost/views.py
@@ -3,6 +3,9 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/home.pt')
 def home(request):
+    """The home route, mapped to the "/" url route
+    Handles our form submission and processing
+    """
     if request.POST:
         post = request.POST.mixed()
         length = int(post['feetLength'])
